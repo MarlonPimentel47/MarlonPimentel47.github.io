ebb2b486021c0a32fbf17e7f181756139a904851
MarlonPimentel47 <mpiment3@binghamton.edu>
2020-02-14 23:17:36 -0500

    new version

---
+++ app.js
@@ -0,0 +1,20 @@
+var bgImageArray = ["./images/2aa.JPG", "./images/3aa.JPG", "./images/4aa.JPG", "./images/5aa.JPG"],
+secs = 4;
+
+bgImageArray.forEach(function(img){
+    new Image().src = img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.documentElement.style.background = "url(" + bgImageArray[k] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
